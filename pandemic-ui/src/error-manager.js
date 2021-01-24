6ba47ae219f2c2078360ded6ffd5b0d255bca9ec
gokalpaltun <gokalpaltun1994@gmail.com>
2021-01-24 11:54:29 +0300

    custom error message integration

--- pandemic-ui/src/error-manager.js
+++ pandemic-ui/src/error-manager.js
@@ -29,10 +29,27 @@ const ERROR_TYPES = {
 class ErrorManager {
   getErrorMessage(errorStatusCode, errorDetaultCode) {
     if (ERROR_TYPES[errorStatusCode][errorDetaultCode].message) {
-      throw new Error(ERROR_TYPES[errorStatusCode][errorDetaultCode].message);
+      const err = new CustomError(
+        ERROR_TYPES[errorStatusCode][errorDetaultCode].message,
+        errorStatusCode
+      );
+      if (err.code === 401) {
+        localStorage.removeItem("state");
+        localStorage.removeItem("graphData");
+      }
+      throw err;
     } else {
       throw new Error("unknown error");
     }
+  }
+}
+
+class CustomError extends Error {
+  constructor(message, code) {
+    super();
+    this.name = "CustomError";
+    this.message = message;
+    this.code = code;
   }
 }
 
